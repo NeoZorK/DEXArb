ff54b4d46cb61414df33dc465bf760f778665422
Ростислав Щербина <neozork@protonmail.com>
2025-03-18 17:28:19 +0200

    add main.h

--- src/main.cpp
+++ src/main.cpp
@@ -6,28 +6,9 @@
 //
 #include "main.h"
 
+
 // Global Project Version
 const std::string VERSION = "102";
-
-/*
-FLAG SCAN:
- -scan 1000, - сканирует фантом (последние 1000 блоков)
- -showSCAN-CONFIG: віводит все настройки конфигурационного файла neozork-config (без учета найденніх данніх после сканирования)
- -showSCAN - вывод всех новіх найденніх данных после сканирования
- -showSCAN-STAT - вівод всех метрик (RAM,CPU, LATENCY и.т.д)
- 
-DEXES:
- -showDEXES - вывод всех найденных после scan и записанных DEX и кол-во (POOLS у каждой)
- -showPOOLS <DEX> - вывод всех пулов в указанной <DEX> + количество
- -showTOKENS <DEX> - вывод всех токенов у DEX (т.к. Нет возможности показать токены - конкретного пула) я если я правильно понял!
- -findTOKEN <DEX> -  вівод информации по токену на вібраннной DEX
- -findTOKENS без указания показівает - все пулі на всех dex, где встречается данній token
- 
- Подготовить readme.md -  детальное описание
- Потом добавить Solana, Ethereium и саміе популярніе
- */
-
-
 //
 //          MAIN FUNCTION
 //
@@ -41,6 +22,7 @@ int main(int argc, char* argv[]) {
     
     // Input Flags
     STRUCT_Input_Flags flags = Parse_Arguments(argc, argv);
+    
     
 
     
