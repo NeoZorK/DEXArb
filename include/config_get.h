fa02c4e11be3fdb3efbe1d05c8f4235bc1cccd06
Ростислав Щербина <neozork@protonmail.com>
2025-03-20 21:20:20 +0200

    add config_get files

---
+++ include/config_get.h
@@ -0,0 +1,13 @@
+//
+//  config_get.hpp
+//  NeoZorKDEXArb
+//
+//  Created by Rostyslav S. on 20.03.2025.
+//
+
+#ifndef config_get_hpp
+#define config_get_hpp
+
+#include <stdio.h>
+
+#endif /* config_get_hpp */
---
+++ src/config_get.cpp
@@ -0,0 +1,8 @@
+//
+//  config_get.cpp
+//  NeoZorKDEXArb
+//
+//  Created by Rostyslav S. on 20.03.2025.
+//
+
+#include "config_get.h"
